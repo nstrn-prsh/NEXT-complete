9b6789b99655d36bb7ff16e19610dcd80f7096ca
nastaran poursharif <nastaran.p.sh.77@gmail.com>
2021-06-30 09:05:18 +0430

    g8- dynamic routing

--- pages/lib/fetch.js
+++ pages/lib/fetch.js
@@ -1,10 +1,17 @@
 import axios from "axios";
 
-const loadPosts = async () => {
+export const loadPosts = async () => {
      // data ro ke az axios migirim esmesho be posts taghiir midim
      const { data: posts } = await axios.get("http://localhost:5000/posts");
 
      return posts;
 };
 
-export default loadPosts;
+export const getPost = async (slug) => {
+     // data ro ke az axios migirim esmesho be posts taghiir midim
+     const { data: posts } = await axios.get("http://localhost:5000/posts", {
+          params: { slug },
+     });
+
+     return posts;
+};
---
+++ pages/posts/[slug].jsx
@@ -0,0 +1,49 @@
+import { useRouter } from "next/dist/client/router";
+import { getPost, loadPosts } from "./../lib/fetch";
+
+const Any = ({ post }) => {
+     const router = useRouter();
+     const { slug } = router.query;
+
+     console.log("slug", slug);
+
+     return (
+          <div>
+               <p>
+                    {/*note: ba estefade az getStaticProps() mitonim "content" post ro daryaft bokonim */}
+                    post slug: {slug}
+               </p>
+               <p>
+                    {/*note: ba estefade az getStaticPaths() mitonim "route" post ro daryaft bokonim */}
+                    content: {post.content}
+               </p>
+          </div>
+     );
+};
+
+export default Any;
+
+export async function getStaticPaths() {
+     const posts = await loadPosts();
+
+     const paths = posts.map((post) => ({
+          params: { slug: post.slug },
+     }));
+
+     return {
+          /*paths: [
+               { params: { slug: 1 } },
+               { params: { slug: 2 } },
+          ],*/
+          paths,
+          fallback: false,
+     };
+}
+
+// be ezaye har masiiri ke match beshe ma yekseri parametr mitonim daryaft konim(params, slug masiri ke match shode ro neshon mide)
+export async function getStaticProps({ params }) {
+     console.log("params", params);
+     const post = await getPost(params.slug);
+
+     return { props: { post: post[0] } };
+}
